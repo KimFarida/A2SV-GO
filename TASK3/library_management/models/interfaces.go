ed4a95775554e56f29458edbd49b6655853a4dda
Kim Farida <farimomoh@gmail.com>
2025-11-14 13:11:44 +0100

    Feat:Included ReserveBook method to Library Manager interface

--- TASK3/library_management/models/interfaces.go
+++ TASK3/library_management/models/interfaces.go
@@ -7,5 +7,6 @@ type LibraryManager interface {
 	ReturnBook(bookID, memberID int)error
 	ListAvailableBooks() []Book
 	ListBorrowed(memberID int) []Book
+	ReserveBook(bookID int, memberID int) error
 
 }
