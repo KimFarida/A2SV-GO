bb3ec2f1f39e1688d238ebd4604a52e3a83988a9
Kim Farida <farimomoh@gmail.com>
2025-11-20 12:53:01 +0100

    Created TaskManager Interface

---
+++ TASK5/task_manager/models/task_manager.go
@@ -0,0 +1,10 @@
+package models
+
+
+type TaskManager interface{ 
+	Add(task Task) error // Create a new task.
+	Get(id string) (Task, error) // Get the details of a specific task.
+	List() (map[string]Task, error) // Get a list of all tasks.
+	Update(id string, task Task) (Task, error) // Update a specific task. 
+	Remove(id string) error //Delete a specific task.
+}
